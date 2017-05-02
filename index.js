d4bc158642538583c26e90f5b1d565ba6c84db24
Julio Olivera <julio.olvr@gmail.com>
2017-05-02 00:37:23 -0300

    Multi language

--- index.js
+++ index.js
@@ -2,17 +2,25 @@ const metalsmith = require('metalsmith')
 const markdown = require('metalsmith-markdown')
 const assets = require('metalsmith-assets')
 const layouts = require('metalsmith-layouts')
+const multiLanguage = require('metalsmith-multi-language');
+
+const DEFAULT_LOCALE = 'en'
+const LOCALES = ['en', 'es']
 
 metalsmith(__dirname)
   .source('src')
   .destination('dist')
+  .use(multiLanguage({
+    default: DEFAULT_LOCALE,
+    locales: LOCALES
+  }))
   .use(markdown())
   .use(assets({
     source: 'assets'
   }))
   .use(layouts({
-    engine: 'jade',
-    default: 'default.jade',
+    engine: 'pug',
+    default: 'default.pug',
     pattern: '**/*.html'
   }))
   .build(err => {
